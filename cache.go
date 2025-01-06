340de5f283f7ce4344ae25e5ed2b60ea112cf4e2
Staphone <staphoneomondi@gmail.com>
2025-01-06 10:05:32 +0300

    file formatting

--- cache.go
+++ cache.go
@@ -2,22 +2,23 @@ package main
 
 import "sync"
 
-type CacheItem struct{
+type CacheItem struct {
 	Value string
 }
 
-type Cache struct{
-	mu sync.RWMutex
+type Cache struct {
+	mu    sync.RWMutex
 	items map[string]CacheItem
 }
 
-func NewCache() *Cache{
+func NewCache() *Cache {
 	return &Cache{
 		items: make(map[string]CacheItem),
 	}
 }
 
-func (c *Cache) Set(key, value string){
+func (c *Cache) Set(key, value string) {
+	// Lock the cache to allow only one instance of writing
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
@@ -26,13 +27,13 @@ func (c *Cache) Set(key, value string){
 	}
 }
 
-func (c *Cache) Get(key string)(string, bool){
+func (c *Cache) Get(key string) (string, bool) {
 	// Only allows for reading operations
 	c.mu.RLock()
 	defer c.mu.Unlock()
-	
+
 	item, found := c.items[key]
-	if !found{
+	if !found {
 		return "", false
 	}
 

f38ea6677aad6984e8288ecd28811efa79302293
Staphone <staphoneomondi@gmail.com>
2025-01-06 10:40:06 +0300

    added cache capacity

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	cs := NewCacheServer()
+	cache := NewCache(5)
+	cache.startEvictionTicker(1 * time.Minute)
+
 	http.HandleFunc("/set", cs.SetHandler)
 	http.HandleFunc("/get", cs.GetHandler)
+	
 	log.Println("Server listening at port: 8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil{

e5e01d144e7f68c646f306adf10fb713ccd036ea
Staphone <staphoneomondi@gmail.com>
2025-01-06 10:12:29 +0300

    fixed mutex read unlocking causing fatal read errors

--- cache.go
+++ cache.go
@@ -30,7 +30,7 @@ func (c *Cache) Set(key, value string) {
 func (c *Cache) Get(key string) (string, bool) {
 	// Only allows for reading operations
 	c.mu.RLock()
-	defer c.mu.Unlock()
+	defer c.mu.RUnlock()
 
 	item, found := c.items[key]
 	if !found {

124e69f6c88b4624e012b31bd3cbfa08a4250f67
Staphone <staphoneomondi@gmail.com>
2025-01-06 10:13:00 +0300

    log statement to show that the server is running on the intended port

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -9,6 +10,7 @@ func main() {
 	cs := NewCacheServer()
 	http.HandleFunc("/set", cs.SetHandler)
 	http.HandleFunc("/get", cs.GetHandler)
+	log.Println("Server listening at port: 8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil{
 		fmt.Println(err)

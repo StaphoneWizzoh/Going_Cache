f55241013769b9b475f4d3a16adf182709a40b87
Staphone <staphoneomondi@gmail.com>
2025-01-06 10:23:34 +0300

    Added TTL eviction policy based on a set timer

--- cache.go
+++ cache.go
@@ -1,9 +1,13 @@
 package main
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 type CacheItem struct {
 	Value string
+	ExpiryTime time.Time
 }
 
 type Cache struct {
@@ -17,13 +21,14 @@ func NewCache() *Cache {
 	}
 }
 
-func (c *Cache) Set(key, value string) {
+func (c *Cache) Set(key, value string, ttl time.Duration) {
 	// Lock the cache to allow only one instance of writing
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
 	c.items[key] = CacheItem{
 		Value: value,
+		ExpiryTime: time.Now().Add(ttl),
 	}
 }
 
@@ -33,9 +38,32 @@ func (c *Cache) Get(key string) (string, bool) {
 	defer c.mu.RUnlock()
 
 	item, found := c.items[key]
-	if !found {
+
+	// If the item is not found or has expired, return false
+	if !found || time.Now().After(item.ExpiryTime){
 		return "", false
 	}
 
 	return item.Value, true
+}
+
+func (c *Cache) startEvictionTicker(d time.Duration){
+	ticker := time.NewTicker(d)
+	go func ()  {
+		for range ticker.C{
+			c.evictExpiredItems()
+		}	
+	}()
+}
+
+func (c *Cache) evictExpiredItems(){
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	now := time.Now()
+	for key, item := range c.items{
+		if now.After(item.ExpiryTime){
+			delete(c.items, key)
+		}
+	}
 }

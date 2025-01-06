cedd6ac5f7de8a4292aa3f63675dc78fce542053
Staphone <staphoneomondi@gmail.com>
2025-01-06 12:04:07 +0300

    go fmt

--- main.go
+++ main.go
@@ -25,10 +25,10 @@ func main() {
 
 	http.HandleFunc("/set", cs.SetHandler)
 	http.HandleFunc("/get", cs.GetHandler)
-	
+
 	log.Println("Server listening at port: 8080")
 	err := http.ListenAndServe(":8080", nil)
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 		return
 	}

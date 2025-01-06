482540d31ca8692bb1aa26732ba4f496e0facc66
Staphone <staphoneomondi@gmail.com>
2025-01-06 10:39:41 +0300

    Added LRU eviction policy logic

--- cache.go
+++ cache.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/list"
 	"sync"
 	"time"
 )
@@ -12,12 +13,21 @@ type CacheItem struct {
 
 type Cache struct {
 	mu    sync.RWMutex
-	items map[string]CacheItem
+	items map[string]*list.Element
+	eviction *list.List
+	capacity int
 }
 
-func NewCache() *Cache {
+type entry struct{
+	key string
+	value CacheItem
+}
+
+func NewCache(capacity int) *Cache {
 	return &Cache{
-		items: make(map[string]CacheItem),
+		items: make(map[string]*list.Element),
+		eviction: list.New(),
+		capacity: capacity,
 	}
 }
 
@@ -26,25 +36,42 @@ func (c *Cache) Set(key, value string, ttl time.Duration) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	c.items[key] = CacheItem{
+	// Remove the old value if it exists
+	if elem, found := c.items[key]; found{
+		c.eviction.Remove(elem)
+		delete(c.items, key)
+	}
+
+	// Evict the least recently used item if the cache is at capacity
+	if c.eviction.Len() >= c.capacity{
+		c.evictLRU()
+	}
+
+	item := CacheItem{
 		Value: value,
 		ExpiryTime: time.Now().Add(ttl),
 	}
+	elem := c.eviction.PushFront(&entry{key, item})
+	c.items[key] = elem
 }
 
 func (c *Cache) Get(key string) (string, bool) {
-	// Only allows for reading operations
 	c.mu.RLock()
-	defer c.mu.RUnlock()
+	defer c.mu.Unlock()
 
-	item, found := c.items[key]
-
-	// If the item is not found or has expired, return false
-	if !found || time.Now().After(item.ExpiryTime){
+	elem, found := c.items[key]
+	if !found || time.Now().After(elem.Value.(*entry).value.ExpiryTime){
+		// If the item is not found or has expired, return false
+		if found{
+			c.eviction.Remove(elem)
+			delete(c.items, key)
+		}
 		return "", false
 	}
 
-	return item.Value, true
+	// Move the accessed element to the front of the eviction list
+	c.eviction.MoveToFront(elem)
+	return elem.Value.(*entry).value.Value, true
 }
 
 func (c *Cache) startEvictionTicker(d time.Duration){
@@ -56,13 +83,23 @@ func (c *Cache) startEvictionTicker(d time.Duration){
 	}()
 }
 
+func (c *Cache) evictLRU(){
+	elem := c.eviction.Back()
+	if elem != nil{
+		c.eviction.Remove(elem)
+		kv := elem.Value.(*entry)
+		delete(c.items, kv.key)
+	}
+}
+
 func (c *Cache) evictExpiredItems(){
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
 	now := time.Now()
-	for key, item := range c.items{
-		if now.After(item.ExpiryTime){
+	for key, elem := range c.items{
+		if now.After(elem.Value.(*entry).value.ExpiryTime){
+			c.eviction.Remove(elem)
 			delete(c.items, key)
 		}
 	}
